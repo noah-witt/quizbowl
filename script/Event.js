4a1ceefbd1d91b9ea23b94762527ff47af2d0fc8
Noah Witt <noah@noah-witt.com>
2017-10-17 19:34:42 -0500

    Team, School Objects

---
+++ script/Event.js
@@ -0,0 +1,6 @@
+function event()
+{
+  this.id = "a";
+}
+event.prototype.id = null;
+event.prototype.schools = [];
---
+++ script/School.js
@@ -0,0 +1,37 @@
+function school()
+{
+  this.id = randomString(9,'aA#');
+}
+school.prototype.numberOfTeams =0;
+school.prototype.name ='';
+school.prototype.teams=[];
+
+school.prototype.setName = function(name)
+{
+  this.name = name;
+  return name;
+};
+
+school.prototype.setNumberOfTeams = function(num)
+{
+  this.numberOfTeams=num;
+  this.teams =[];
+  for(var i=1;i<=num;i++)
+  {
+    this.addTeam(i);
+  }
+};
+
+school.prototype.addTeam = function(num)
+{
+  this.teams.push(new team(this,num));
+};
+
+function team(school,number)
+{
+  this.id = randomString(9,'aA#');
+  this.school = school;
+  this.teamNumber = number;
+}
+team.prototype.school = null;
+team.prototype.teamNumber = 0;
