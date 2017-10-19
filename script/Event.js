a2d27b6df38bcefb5b12ef0f447e93325c302408
Noah Witt <noah@noah-witt.com>
2017-10-18 20:34:43 -0500

    Generate Randomized list

--- script/Event.js
+++ script/Event.js
@@ -27,3 +27,21 @@ event.prototype.getNumberOfSchools = function()
 {
   return this.numberOfSchools;
 };
+
+event.prototype.getOrderedListOfTeams = function()
+{
+  var list =[];
+  for(i=0;i<this.schools.length;i++)
+  {
+    //console.log(this.schools[i].teams);
+    list = list.concat(this.schools[i].teams);
+  }
+  return list;
+};
+
+//returns shuffled list of teams
+event.prototype.getRandomizedListOfTeams = function()
+{
+  var arr = this.getOrderedListOfTeams();
+  return shuffle(arr);
+};
