09141be9b97ca587e10bc7fc806c262efce2ec4e
Noah Witt <noah@noah-witt.com>
2017-11-02 18:33:05 -0500

    configs

--- script/init.js
+++ script/init.js
@@ -9,6 +9,7 @@ window.config = {
   isDone:false,
   isEven:false,
   rounds:6,
+  showOG: false,
   scheduleFooters : '<div class="panel-footer">Schedule created using quizbowl.noah-witt.com.</div>',
   numberOfDigitsForRandomStrings: 20
 };
